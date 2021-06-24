4cc827186f0f1650b9872556b7a05ee4761c20da
Mihalache Ștefan Cristian <57230349+StefanCsPurge@users.noreply.github.com>
2021-06-24 12:26:28 +0300

    asp posts model 2020 damn done

---
+++ Exam_practice/asp-posts-2020/Controllers/ApiController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using asp_app.DataAbstractionLayer;
+using asp_app.Models;
+using System.Diagnostics;
+using MySql.Data.MySqlClient;
+
+namespace asp_app.Controllers
+{
+    public class ApiController : Controller
+    {
+        private static Boolean postAdded = false;
+        private static String userThatAdded = "";
+        private static String addedTopicId = "";
+        private static String addedText = "";
+        private static String addedDate = "";
+
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View("MainPage");
+        }
+
+        public string Test()
+        {
+            return "It's working";
+        }
+
+        public string GetAllPosts()
+        {
+            DAL dal = new DAL();
+            string result = "<table border=1><thead><th>ID</th><th>User</th><th>TopicID</th><th>Text</th><th>Date</th></thead>";
+            try
+            {
+                MySqlConnection conn = dal.getConnection();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from posts";
+                MySqlDataReader myreader = cmd.ExecuteReader();
+                while (myreader.Read())
+                {
+                    result += "<tr><td>" + myreader.GetString("id") + "</td><td>" 
+                        + myreader.GetString("user") + "</td><td>" 
+                        + myreader.GetString("topicid") + "</td><td>" 
+                        + myreader.GetString("text") + "</td><td>" 
+                        + myreader.GetString("date") + "</td></tr>";
+                }
+                myreader.Close();
+                conn.Close();
+            }
+            catch (MySqlException e)
+            {
+                Debug.Write(e);
+            }
+            result += "</table>";
+            return result;
+        }
+
+        public string CheckNotification()
+        {
+            string user = Request.Params["user"];
+            if (postAdded && user != userThatAdded)
+            {
+                postAdded = false;
+                return "User " + userThatAdded + " added the article ->\n" +
+                        "TopicID: '" + addedTopicId + "', text: '" + addedText + "', date: '" + addedDate + "'";
+            }
+            return "";
+        }
+
+
+        public void UpdatePost()
+        {
+            String user = Request.Params["user"];
+            String postId = Request.Params["postid"];
+            String topicName = Request.Params["topicname"];
+            String text = Request.Params["text"];
+            String date = Request.Params["date"];
+            DAL dal = new DAL();
+            
+            String topicId = dal.findTopicId(topicName);
+            if (topicId == null)
+            {
+                dal.addTopic(topicName);
+                topicId = dal.findTopicId(topicName);
+            }
+            dal.updatePost(postId, user, topicId, text, date);
+        }
+
+        public void AddPost()
+        {
+            String user = Request.Params["user"];
+            String topicName = Request.Params["topicname"];
+            String text = Request.Params["text"];
+            String date = Request.Params["date"];
+            DAL dal = new DAL();
+
+            String topicId = dal.findTopicId(topicName);
+            if (topicId == null)
+            {
+                dal.addTopic(topicName);
+                topicId = dal.findTopicId(topicName);
+            }
+            dal.addPost(user, topicId, text, date);
+
+            postAdded = true;
+            userThatAdded = user;
+            addedTopicId = topicId;
+            addedText = text;
+            addedDate = date;
+
+        }
+    }
+}
---
+++ Exam_practice/asp-posts-2020/DataAbstractionLayer/DAL.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace asp_app.DataAbstractionLayer
+{
+    public class DAL
+    {
+        public MySqlConnection getConnection()
+        {
+            string myConnectionString;
+            myConnectionString = "server=localhost;uid=root;pwd=;database=phplab;";
+            return new MySqlConnection(myConnectionString);
+        }
+
+        public bool login(string user)
+        {
+            List<String> users = new List<String>();
+            try
+            {
+                MySqlConnection conn = getConnection();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from persons where name='" + user + "'";
+                MySqlDataReader myreader = cmd.ExecuteReader();
+                while (myreader.Read())
+                {
+                    users.Add(myreader.GetString("name"));
+                }
+                conn.Close();
+                myreader.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Debug.Write(ex.Message);
+                return false;
+            }
+            return users.Count == 1;
+        }
+
+        public int GetUserIdOfUser(string user)
+        {
+            int report = 0;
+            try
+            {
+                MySqlConnection conn = getConnection();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select id from persons where name='" + user + "'";
+                MySqlDataReader myreader = cmd.ExecuteReader();
+                while (myreader.Read())
+                {
+                    report = myreader.GetInt32("id");
+                }
+                myreader.Close();
+                conn.Close();
+            }
+            catch (MySqlException e)
+            {
+                Debug.Write(e);
+            }
+            return report;
+        }
+
+        public string findTopicId(string topicName)
+        {
+            String id = null;
+            try
+            {
+                MySqlConnection conn = getConnection();
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select id from topics where topicname='" + topicName + "'";
+                MySqlDataReader myreader = cmd.ExecuteReader();
+                if (myreader.Read())
+                {
+                    id = myreader.GetString("id");
+                }
+                myreader.Close();
+                conn.Close();
+            }
+            catch (MySqlException e)
+            {
+                Debug.Write(e);
+            }
+            return id;
+        }
+
+        public void addTopic(string topicName)
+        {
+            MySqlConnection conn = getConnection();
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT INTO topics(topicname) VALUES ('" + topicName + "')";
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Debug.Write(ex.Message);
+            }
+            conn.Close();
+        }
+
+        public void updatePost(String postId, String username, String topicId, String text, String cdate)
+        {
+            MySqlConnection conn = getConnection();
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "UPDATE posts SET user='" + username +
+                "', topicid='" + topicId +
+                "', text='" + text +
+                "', date='" + cdate + "' WHERE id='" + postId + "'";
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Debug.Write(ex.Message);
+            }
+            conn.Close();
+        }
+
+        public void addPost(String username, String topicId, String text, String cdate)
+        {
+            MySqlConnection conn = getConnection();
+            conn.Open();
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT INTO posts(user, topicid, text, date) " +
+                "VALUES ('" + username + "','" + topicId + "','" + text + "','" + cdate + "')";
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException ex)
+            {
+                Debug.Write(ex.Message);
+            }
+            conn.Close();
+        }
+
+    }
+}
